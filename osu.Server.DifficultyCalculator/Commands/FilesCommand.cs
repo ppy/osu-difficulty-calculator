dcf0d22b72766cdf4349a3a18b1e17b75bf199de
smoogipoo <smoogipoo@smgi.me>
2019-01-03 14:59:26 +0900

    Add "files" command, process files in directory

---
+++ osu.Server.DifficultyCalculator/Commands/FilesCommand.cs
@@ -0,0 +1,28 @@
+// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System.Collections.Generic;
+using System.IO;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace osu.Server.DifficultyCalculator.Commands
+{
+    [Command(Name = "files", Description = "Computes the difficulty of all files in the beatmaps path.")]
+    public class FilesCommand : CalculatorCommand
+    {
+        protected override IEnumerable<int> GetBeatmaps()
+        {
+            var ids = new List<int>();
+
+            foreach (var f in Directory.GetFiles(AppSettings.BeatmapsPath))
+            {
+                var filename = Path.GetFileNameWithoutExtension(f);
+
+                if (int.TryParse(filename.Split('.')[0], out var id))
+                    ids.Add(id);
+            }
+
+            return ids;
+        }
+    }
+}
