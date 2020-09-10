3e058feb25606ea872ae979b756a4a9807109370
smoogipoo <smoogipoo@smgi.me>
2020-09-10 18:34:55 +0900

    Temporarily disable health reporting

--- osu.Server.DifficultyCalculator/Commands/CalculatorCommand.cs
+++ osu.Server.DifficultyCalculator/Commands/CalculatorCommand.cs
@@ -166,7 +166,7 @@ namespace osu.Server.DifficultyCalculator.Commands
         private void outputHealth()
         {
             var process = Process.GetCurrentProcess();
-            reporter.Verbose($"Health p:{process.PrivateMemorySize64.Bytes()} v:{process.VirtualMemorySize64.Bytes()} w:{process.WorkingSet64.Bytes()}");
+            //reporter.Verbose($"Health p:{process.PrivateMemorySize64.Bytes()} v:{process.VirtualMemorySize64.Bytes()} w:{process.WorkingSet64.Bytes()}");
 
             string threadsString = string.Empty;
             for (int i = 0; i < threadBeatmapIds.Length; i++)
